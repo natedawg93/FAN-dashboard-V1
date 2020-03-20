db96200702ef89fc52075385d054e78d108c82e4
natarajan1993 <natrajm93@gmail.com>
2020-03-20 09:02:44 -0500

    Updated settings and gitignore

--- fan_dashboard/settings.py
+++ fan_dashboard/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3fb^cfb+tuit&*o5vms+2n8b8jb8b!ygu%4z%wp1(b+j3thdf%'
+# SECRET_KEY = '3fb^cfb+tuit&*o5vms+2n8b8jb8b!ygu%4z%wp1(b+j3thdf%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
