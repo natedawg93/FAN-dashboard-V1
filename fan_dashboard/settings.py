224404122079fe2ca1fefb97bffec7595590f4a9
natarajan1993 <natrajm93@gmail.com>
2020-03-20 12:49:28 -0500

    Reload window on websocket close

--- fan_dashboard/settings.py
+++ fan_dashboard/settings.py
@@ -25,7 +25,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
-print(os.environ.get('DEBUG_VALUE'))
 
 ALLOWED_HOSTS = ['localhost','freshideasfandashboard.herokuapp.com']
 
