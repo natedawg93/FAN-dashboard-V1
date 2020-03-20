3512c16b64fefbf6e9de8f20ba3bf290ff0ab702
natarajan1993 <natrajm93@gmail.com>
2020-03-20 10:09:19 -0500

    Changed Debug value

--- fan_dashboard/settings.py
+++ fan_dashboard/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['freshideasfandashboard.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','freshideasfandashboard.herokuapp.com']
 
 
 # Application definition
